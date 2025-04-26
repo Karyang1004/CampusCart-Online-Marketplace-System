41043cc4a355a8e7cb79a0fdf83fed11118b2b14
Karen Lean Kay Cabarrubias <karenleankay.cabarrubias@cit.edu>
2025-04-27 03:23:47 +0800

    changed configurations for frontend deployment

--- backend/src/main/java/edu/cit/campuscart/config/SecurityConfig.java
+++ backend/src/main/java/edu/cit/campuscart/config/SecurityConfig.java
@@ -56,13 +56,13 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
          // Allow all origins for testing
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://accounts.google.com"));
+        //configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://accounts.google.com", "https://*.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
          // Set allowCredentials to false when using "*" for origins
-        configuration.setAllowCredentials(false);
-        //configuration.setAllowCredentials(true);
+        //configuration.setAllowCredentials(false);
+        configuration.setAllowCredentials(true);
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
         configuration.setMaxAge(3600L);
         
